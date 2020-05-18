aecf83a1804877b028b9090c8d03af61a572e31a
Sergey Li <pincats@gmail.com>
2020-05-18 23:39:36 +0300

    feat: Add Movie class

--- app/src/main/java/com/example/android/popularmovies/Movie.java
+++ app/src/main/java/com/example/android/popularmovies/Movie.java
@@ -2,14 +2,14 @@ package com.example.android.popularmovies;
 
 class Movie {
     private String title;
-    private int posterImage;
+    private String posterUrl;
     private String overview;
     private int userRating;
     private String releaseDate;
 
-    public Movie(String title, int posterImage, String overview, int userRating, String releaseDate) {
+    public Movie(String title, String posterUrl, String overview, int userRating, String releaseDate) {
         this.title = title;
-        this.posterImage = posterImage;
+        this.posterUrl = posterUrl;
         this.overview = overview;
         this.userRating = userRating;
         this.releaseDate = releaseDate;
@@ -19,24 +19,12 @@ class Movie {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getPosterImage() {
-        return posterImage;
-    }
-
-    public void setPosterImage(int posterImage) {
-        this.posterImage = posterImage;
+    public String getPosterImage() {
+        return posterUrl;
     }
 
     public String getOverview() {
         return overview;
-    }
-
-    public void setOverview(String overview) {
-        this.overview = overview;
     }
 
     public int getUserRating() {
@@ -51,15 +39,11 @@ class Movie {
         return releaseDate;
     }
 
-    public void setReleaseDate(String releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
     @Override
     public String toString() {
         return "Movie{" +
                 "title='" + title + '\'' +
-                ", posterImage=" + posterImage +
+                ", posterImage=" + posterUrl +
                 ", overview='" + overview + '\'' +
                 ", userRating=" + userRating +
                 ", releaseDate='" + releaseDate + '\'' +
