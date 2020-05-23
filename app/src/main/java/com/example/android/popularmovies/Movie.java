0f0b5c449724da1ffe62bf911ca276582f96fd8c
Sergey Li <pincats@gmail.com>
2020-05-23 21:47:50 +0300

    bug: Read from parcel in order fields were written to it

--- app/src/main/java/com/example/android/popularmovies/Movie.java
+++ app/src/main/java/com/example/android/popularmovies/Movie.java
@@ -4,18 +4,30 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 class Movie implements Parcelable {
+    public static final String EXTRA_MOVIE_PARCELABLE = "Movie";
+
     private String title;
     private String posterUrl;
     private String overview;
     private int userRating;
     private String releaseDate;
 
-    public Movie(String title, String posterUrl, String overview, int userRating, String releaseDate) {
+    Movie(String title, String posterUrl, String overview, int userRating, String releaseDate) {
         this.title = title;
         this.posterUrl = posterUrl;
         this.overview = overview;
         this.userRating = userRating;
         this.releaseDate = releaseDate;
+    }
+
+    private Movie(Parcel in) {
+        String[] fieldsArray = new String[4];
+        in.readStringArray(fieldsArray);
+        this.title = fieldsArray[0];
+        this.posterUrl = fieldsArray[1];
+        this.overview = fieldsArray[2];
+        this.releaseDate = fieldsArray[3];
+        this.userRating = in.readInt();
     }
 
     public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
@@ -61,16 +73,6 @@ class Movie implements Parcelable {
                 ", userRating=" + userRating +
                 ", releaseDate='" + releaseDate + '\'' +
                 '}';
-    }
-
-    public Movie(Parcel in) {
-        this.userRating = in.readInt();
-        String[] fieldsArray = new String[4];
-        in.readStringArray(fieldsArray);
-        this.title = fieldsArray[0];
-        this.posterUrl = fieldsArray[1];
-        this.overview = fieldsArray[2];
-        this.releaseDate = fieldsArray[3];
     }
 
     @Override
