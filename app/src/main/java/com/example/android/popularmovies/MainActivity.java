324a913ad2afec8627c4d9edabecdcc436ab7b98
Sergey Li <pincats@gmail.com>
2020-05-24 19:21:42 +0300

    feat: Retrieve movies data from themoviedb

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,42 +1,42 @@
 package com.example.android.popularmovies;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.android.popularmovies.utilities.FakeMovieData;
 import com.example.android.popularmovies.utilities.MovieJasonUtils;
+import com.example.android.popularmovies.utilities.NetworkUtilities;
 
-import org.json.JSONException;
-
+import java.net.URL;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements MovieAdapter.OnMovieClickListener {
 
     RecyclerView mRecyclerView;
+    MovieAdapter mMovieAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            List<Movie> movies = MovieJasonUtils.createFromJsonString(FakeMovieData.getFakeMovieJsonString());
-            mRecyclerView = findViewById(R.id.rv_movies_grid);
-            RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
-            mRecyclerView.setLayoutManager(layoutManager);
-            mRecyclerView.setHasFixedSize(true);
+        mRecyclerView = findViewById(R.id.rv_movies_grid);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
 
-            MovieAdapter adapter = new MovieAdapter(this);
-            adapter.setMoviesData(movies);
-            mRecyclerView.setAdapter(adapter);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        mMovieAdapter = new MovieAdapter(this);
+        mRecyclerView.setAdapter(mMovieAdapter);
 
+        loadMovieData();
+    }
+
+    private void loadMovieData() {
+        new FetchMovieClass().execute("movie/top_rated");
     }
 
     @Override
@@ -44,5 +44,40 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.OnMo
         Intent openMovieDetailsIntent = new Intent(this, MovieDetailsActivity.class);
         openMovieDetailsIntent.putExtra(Movie.EXTRA_MOVIE_PARCELABLE, movie);
         startActivity(openMovieDetailsIntent);
+    }
+
+    public class FetchMovieClass extends AsyncTask<String, Void, List<Movie>> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected List<Movie> doInBackground(String... params) {
+            if (params != null && params.length == 0) {
+                return null;
+            }
+
+            String requestEndpoint = params[0];
+            URL movieUrl = NetworkUtilities.buildURL(requestEndpoint);
+
+            List<Movie> movies = null;
+            try {
+                String jsonResponse = NetworkUtilities.getResponseFromHttpUrl(movieUrl);
+                movies = MovieJasonUtils.createFromJsonString(jsonResponse);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return movies;
+        }
+
+        @Override
+        protected void onPostExecute(List<Movie> movies) {
+            if (movies != null) {
+                mMovieAdapter.setMoviesData(movies);
+                mMovieAdapter.notifyDataSetChanged();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/android/popularmovies/utilities/NetworkUtilities.java
+++ app/src/main/java/com/example/android/popularmovies/utilities/NetworkUtilities.java
@@ -4,13 +4,17 @@ import android.net.Uri;
 
 import com.example.android.popularmovies.BuildConfig;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public final class NetworkUtilities {
     private static final String TAG = NetworkUtilities.class.getSimpleName();
 
-    private static final String THEMOVIEDB_BASE_URL = "http://api.themoviedb.org/3";
+    private static final String THEMOVIEDB_BASE_URL = "https://api.themoviedb.org/3";
     private static final String API_KEY_QUERY = "api_key";
 
     public static URL buildURL(String endpoint) {
@@ -29,7 +33,21 @@ public final class NetworkUtilities {
         return url;
     }
 
-    public static String getResponseFromHttpUrl(URL url) {
-        return null;
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = connection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            connection.disconnect();
+        }
     }
 }
