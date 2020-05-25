cac083a4475709baa77996cf5043de91e7432314
Sergey Li <pincats@gmail.com>
2020-05-26 00:53:55 +0300

    feat: Change poster size to smaller

--- app/src/main/java/com/example/android/popularmovies/utilities/MovieJasonUtils.java
+++ app/src/main/java/com/example/android/popularmovies/utilities/MovieJasonUtils.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public final class MovieJasonUtils {
     private static final String POSTER_BASE_URL = "https://image.tmdb.org/t/p/";
-    private static final String POSTER_SIZE = "w342";
+    private static final String POSTER_SIZE = "w185"; // recommended
 
     private static final String KEY_RESULTS = "results";
     private static final String KEY_TITLE = "original_title";
